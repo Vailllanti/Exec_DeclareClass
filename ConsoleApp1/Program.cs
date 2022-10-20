d58b7cdf5ea3d18e9b074e07e5ac3466581ab230
建均 陳 <protosscarrier@gmail.com>
2022-10-20 17:02:34 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+	class Address
+	{
+		public string city { get; set; }
+		public string Zipcode { get; set; }
+		public string street { get; set; }
+	}
+	
+	class Telephone
+	{
+		public string CountryCode { get; set; }
+		public string AreaCode { get; set; }
+		public string Number { get; set; }
+		public string Ext { get; set; }
+	}
+
+	class Member
+	{
+		public string Name { get; set; }
+		public string Account { get; set; }
+		public string Password { get; set; }
+		public string Email { get; set; }
+		public Address Address { get; set; }
+		public Telephone Cellphnone { get; set; }
+		public void Register(string name, string account, string password, string confirmPassword, string email) { }
+		public void ForgetPassword(string name, string email) { }
+		public void Authenticate(string account, string password) { }
+	}
+}
